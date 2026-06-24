45b49ef92085ffa779710947783d132b01a29586
Cyberclutch146 <blazingswagata@gmail.com>
2026-06-24 23:22:03 +0530

    build: initialize Android project configuration with namespace workaround and build directory customization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.freelanceflow.freelance_flow"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
